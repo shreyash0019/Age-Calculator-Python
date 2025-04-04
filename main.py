f142fdfae9a7ce6e4598951104e158a8b174120c
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-04-04 23:44:27 +0530

    Indentation update

--- main.py
+++ main.py
@@ -1,75 +1,42 @@
 import time
-
 from calendar import isleap
 
 # judge the leap year
-
 def judge_leap_year(year):
-
- if isleap(year):
-
- return True
-
- else:
-
- return False
+    return isleap(year)
 
 # returns the number of days in each month
-
 def month_days(month, leap_year):
-
- if month in [1, 3, 5, 7, 8, 10, 12]:
-
- return 31
-
- elif month in [4, 6, 9, 11]:
-
- return 30
-
- elif month == 2 and leap_year:
-
- return 29
-
- elif month == 2 and (not leap_year):
-
- return 28
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2 and leap_year:
+        return 29
+    elif month == 2 and not leap_year:
+        return 28
 
 name = input("input your name: ")
-
-age = input("input your age: ")
+age = int(input("input your age: "))
 
 localtime = time.localtime(time.time())
-
-year = int(age)
-
+year = age
 month = year * 12 + localtime.tm_mon
-
 day = 0
-
-begin_year = int(localtime.tm_year) - year
-
+begin_year = localtime.tm_year - year
 end_year = begin_year + year
 
 # calculate the days
-
 for y in range(begin_year, end_year):
-
- if (judge_leap_year(y)):
-
- day = day + 366
-
- else:
-
- day = day + 365
+    if judge_leap_year(y):
+        day += 366
+    else:
+        day += 365
 
 leap_year = judge_leap_year(localtime.tm_year)
-
 for m in range(1, localtime.tm_mon):
+    day += month_days(m, leap_year)
 
- day = day + month_days(m, leap_year)
+day += localtime.tm_mday
 
-day = day + localtime.tm_mday
-
-print("%s's age is %d years or " % (name, year), end="")
-
-print("%d months or %d days" % (month, day))
+print(f"{name}'s age is {year} years or {month} months or {day} days")
